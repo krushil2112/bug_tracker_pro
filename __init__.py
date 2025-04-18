4a758bfb2c32dc90ee820e730e2fa80999e66f99
krushil2112 <patel9zb@uwindsor.ca>
2025-04-18 04:47:18 -0400

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,24 +1,4 @@
-"""distutils
-
-The main package for the Python Module Distribution Utilities.  Normally
-used from a setup script as
-
-   from distutils.core import setup
-
-   setup (...)
-"""
-
-import sys
-import importlib
-
-__version__ = sys.version[: sys.version.index(' ')]
-
-
-try:
-    # Allow Debian and pkgsrc (only) to customize system
-    # behavior. Ref pypa/distutils#2 and pypa/distutils#16.
-    # This hook is deprecated and no other environments
-    # should use it.
-    importlib.import_module('_distutils_system_mod')
-except ImportError:
-    pass
+from .serving import run_simple as run_simple
+from .test import Client as Client
+from .wrappers import Request as Request
+from .wrappers import Response as Response
